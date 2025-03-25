1bd664cf3e5a3970c3e5aa232b1b959eafcc33cd
Dozy-y <igorkazanov1706@gmail.com>
2025-03-25 22:05:43 +0300

    Update 2.1.sql

--- 2.1.sql
+++ 2.1.sql
@@ -6,28 +6,28 @@ INSERT INTO Employee (
     position, 
     salary
 ) VALUES (
-    'Anna', 
-    'Smith', 
+    'Анна', 
+    'Иванова', 
     '1995-03-25', 
     '2023-01-10', 
-    'Trainer', 
+    'Тренер', 
     3000.00
 );
 
 INSERT INTO Employee 
 VALUES (
     DEFAULT, 
-    'Mike', 
-    'Brown', 
+    'Михаил', 
+    'Петров', 
     '1988-07-12', 
     '2020-05-15', 
-    'Manager', 
+    'Менеджер', 
     4500.00, 
-    '+123456789', 
-    'mike@example.com', 
-    'London', 
-    1, -- supervisor_id
-    'Training', 
+    '+79161234567', 
+    'mikhail.petrov@example.com', 
+    'Москва', 
+    1, 
+    'Тренинг', 
     3
 );
 
@@ -36,14 +36,14 @@ SET salary = 3200.00
 WHERE employee_id = 5;
 
 UPDATE Employee
-SET department = 'Junior Training'
-WHERE position = 'Trainer';
+SET department = 'Младшие тренеры'
+WHERE position = 'Тренер';
 
 DELETE FROM Employee
 WHERE employee_id = 10;
 
 DELETE FROM Employee
-WHERE department = 'Archived';
+WHERE department = 'Архив';
 
 SELECT * FROM Employee
 WHERE hire_date > '2020-01-01';
